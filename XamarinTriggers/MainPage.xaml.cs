f23ff99ba34f036d7e0b8f89a9ca06c1bf68fc10
jorgediegocrespo <jorgediegocrespo@gmail.com>
2019-02-04 14:01:37 +0100

    Xamarin triggers demo

---
+++ XamarinTriggers/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+namespace XamarinTriggers
+{
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    {
+        private string _name;
+        private string _surname;
+
+        public string Name 
+        { 
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged(); 
+            }
+        }
+
+        public string Surname
+        {
+            get { return _surname; }
+            set
+            {
+                _surname = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainPage()
+        {
+            Name = string.Empty;
+            Surname = string.Empty;
+
+            InitializeComponent();
+            BindingContext = this;
+        }
+    }
+}
